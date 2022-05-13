a105cc4c6febbc79c83922c393621d5dbaf8046a
MixonUa <mykhailofrolov@gmail.com>
2022-05-13 18:34:02 +0300

    add visual content

---
+++ TestEmailApp/TestEmailApp/Views/Cells/MailCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  MailCollectionViewCell.swift
+//  TestEmailApp
+//
+//  Created by Михаил Фролов on 13.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class MailCollectionViewCell: UICollectionViewCell {
+    private let domainLable: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.backgroundColor = .white
+        contentView.alpha = 0.5
+        contentView.layer.cornerRadius = 10
+        //меняем именно у ContentView а не просто у ячейки
+        
+        addSubview(domainLable)
+    }
+}
+
+extension MailCollectionViewCell {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            domainLable.centerXAnchor.constraint(equalTo: centerXAnchor),
+            domainLable.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
